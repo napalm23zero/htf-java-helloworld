2f68c50554a622fe0da772e8b0f1486f47eb0103
Rodrigo Dantas <rodrigo.dantas@hustletech.dev>
2023-07-04 01:41:08 +0000

    simple hello-world

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Esta é uma classe simples que imprime a frase "Hello World"
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
